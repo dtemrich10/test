6e2ca1a8d5e2fa51e2eaacee9569903225e019d5
David <dtemrich@gmail.com>
2021-10-05 13:50:56 -0500

    this is sue

--- src/com/emrich/Main.java
+++ src/com/emrich/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void sayHello() {
         // The end line character happens automatically when we use
         // the println() function.
-        System.out.println("Hello World!");
+        System.out.println("Hello My name is sue");
     }
 
     public static void main(String[] args) {
